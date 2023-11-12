4a491414e6cbcea9e17043d60e91cefb404a0656
Vlad Maslennikov <vlad.maslennikov@gmail.com>
2023-11-12 14:13:49 +0300

    Initial Commit

---
+++ api-gateway/src/main/java/com/example/gateway/GatewayRouteConfig.java
@@ -0,0 +1,22 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GatewayRouteConfig {
+
+    @Bean
+    public RouteLocator gatewayRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+            .route("75b3f45e-bc35-34f2-a113-3e8a85bb2ccf", route -> route
+                .path("/**")
+                .uri("http://localhost:8082"))
+
+            .build();
+    }
+}
